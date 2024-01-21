822bf30fcd9912fbf5a954d248e00a4b8e1e1649
SHUVAM KUMAR SINGH <118374131+MrIncursio03@users.noreply.github.com>
2024-01-21 20:09:35 +0530

    Create 21-01-2024.py

---
+++ 2024/01 January 2024/21-01-2024.py
@@ -0,0 +1,26 @@
+"""
+
+Author : Shuvam Kumar Singh
+Date : 21/01/2024
+Problem : 198-House Robber
+Difficulty : Medium
+
+"""
+
+
+class Solution:
+  def rob(self, nums: List[int]) -> int:
+    if not nums:
+      return 0
+    if len(nums) == 1:
+      return nums[0]
+
+    # dp[i]:= max money of robbing nums[0..i]
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+      dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+
+    return dp[-1]
