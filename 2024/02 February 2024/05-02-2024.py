e1d65ef486a1ca4f0f98e7b3063d0b2dd2108ad3
SHUVAM KUMAR SINGH <118374131+MrIncursio03@users.noreply.github.com>
2024-02-05 23:40:53 +0530

    Create 05-02-2024.py

---
+++ 2024/02 February 2024/05-02-2024.py
@@ -0,0 +1,24 @@
+"""
+
+Author : Shuvam Kumar Singh
+Date : 04/02/2024
+Problem : 387. First Unique Character in a String
+Difficulty : Easy
+
+"""
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        # build hash map: character and how often it appears
+        count = collections.Counter(s)
+        
+        # find the index
+        for idx, ch in enumerate(s):
+            if count[ch] == 1:
+                return idx     
+        return -1
