72f9a69c67ca47b527b1afc5c52a132400f25bdb
SHUVAM KUMAR SINGH <118374131+MrIncursio03@users.noreply.github.com>
2024-09-01 20:29:28 +0530

    Update 01-09-2024.java

--- 2024/09 September 2024/01-09-2024.java
+++ 2024/09 September 2024/01-09-2024.java
@@ -1,1 +1,19 @@
-Soon
+/*
+Author : Shuvam Kumar Singh
+Date : 01/09/2024
+Problem : 2022. Convert 1D Array Into 2D Array
+Difficulty : Easy
+*/
+  
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (m * n != original.length) return new int[0][0];
+        int[][] ans = new int[m][n];
+        
+        for (int i = 0; i < original.length; i++) {
+            ans[i / n][i % n] = original[i];
+        }
+        
+        return ans;
+    }
+}
